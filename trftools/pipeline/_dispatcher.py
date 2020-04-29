917392a213085d011c6cea2af81c23c741fcbd0e
Christian Brodbeck <christianmbrodbeck@gmail.com>
2020-04-29 08:39:21 -0400

    Dispatcher terminal width guess

--- trftools/pipeline/_dispatcher.py
+++ trftools/pipeline/_dispatcher.py
@@ -548,7 +548,7 @@ class Dispatcher(object):
 
     def show_jobs(self, trfs=False, width=None):
         if width is None:
-            width = shutil.get_terminal_size((150, 20))[0] - 50
+            width = shutil.get_terminal_size((150, 20))[0] - 55
         pending_jobs = {job.path: job for job in self.server.pending_jobs()}
         priority = len({job.priority for job in self._user_jobs}) > 1
         t = fmtxt.Table('lllrrl' + 'l' * priority)
