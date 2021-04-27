dd93f81e6a3121d75ac8a068cf9f8692b3ac3761
Christian Brodbeck <christianmbrodbeck@gmail.com>
2021-04-26 21:38:11 -0400

    OPT gammatone_bank:  parallelize and reduce memory footprint

--- trftools/_sound.py
+++ trftools/_sound.py
@@ -1,10 +1,35 @@
 # Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
+from math import floor
+
 from eelbrain import NDVar, Scalar, UTS
+from eelbrain._utils import tqdm
+from numba import njit, prange
+import numpy as np
 
 from ._ndvar import pad as _pad_func
 
 
-def gammatone_bank(wav: NDVar, f_min: float, f_max: float, n: int, integration_window: float = 0.010, tstep: float = None, location: str = 'right', pad: bool = True, name: str = None) -> NDVar:
+@njit(parallel=True)
+def aggregate(xf: np.ndarray, n_samples: int, step: float, n_window: int):
+    xf **= 2
+    out = np.empty(n_samples)
+    for i in prange(n_samples):
+        start = int(round(i * step))
+        out[i] = xf[start: start + n_window].mean()
+    return out
+
+
+def gammatone_bank(
+        wav: NDVar,
+        f_min: float,
+        f_max: float,
+        n: int,
+        integration_window: float = 0.010,
+        tstep: float = None,
+        location: str = 'right',
+        pad: bool = True,
+        name: str = None,
+) -> NDVar:
     """Gammatone filterbank response
 
     Parameters
@@ -42,10 +67,9 @@ def gammatone_bank(wav: NDVar, f_min: float, f_max: float, n: int, integration_w
 
         $ pip install https://github.com/christianbrodbeck/gammatone/archive/fmax.zip
     """
-    from gammatone.filters import centre_freqs
-    from gammatone.gtgram import gtgram
+    from gammatone.filters import centre_freqs, erb_filterbank
+    from gammatone.gtgram import make_erb_filters
 
-    tmin = wav.time.tmin
     wav_ = wav
     if location == 'left':
         if pad:
@@ -61,12 +85,24 @@ def gammatone_bank(wav: NDVar, f_min: float, f_max: float, n: int, integration_w
             wav_ = _pad_func(wav, wav.time.tmin - dt, wav.time.tstop + dt)
     else:
         raise ValueError(f"mode={location!r}")
-    sfreq = 1 / wav.time.tstep
+    fs = 1 / wav.time.tstep
     if tstep is None:
         tstep = wav.time.tstep
-    x = gtgram(wav_.get_data('time'), sfreq, integration_window, tstep, n, f_min, f_max)
-    freqs = centre_freqs(sfreq, n, f_min, f_max)
-    # freqs = np.round(freqs, out=freqs).astype(int)
-    freq_dim = Scalar('frequency', freqs[::-1], 'Hz')
-    time_dim = UTS(tmin, tstep, x.shape[1])
-    return NDVar(x, (freq_dim, time_dim), name or wav.name)
+    wave = wav_.get_data('time')
+    # based on gammatone library, rewritten to reduce memory footprint
+    cfs = centre_freqs(fs, n, f_min, f_max)
+    integration_window_len = int(round(integration_window * fs))
+    output_n_samples = floor((len(wave) - integration_window_len) * wav.time.tstep / tstep)
+    output_step = tstep / wav.time.tstep
+    results = []
+    for i, cf in tqdm(enumerate(reversed(cfs)), "Gammatone spectrogram", total=len(cfs), unit='band'):
+        fcoefs = np.flipud(make_erb_filters(fs, cf))
+        xf = erb_filterbank(wave, fcoefs)
+        results.append(aggregate(xf[0], output_n_samples, output_step, integration_window_len))
+    result = np.sqrt(results)
+    # package output
+    freq_dim = Scalar('frequency', cfs[::-1], 'Hz')
+    time_dim = UTS(wav.time.tmin, tstep, output_n_samples)
+    if name is None:
+        name = wav.name
+    return NDVar(result, (freq_dim, time_dim), name)
