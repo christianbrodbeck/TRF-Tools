cd282324a6523629a810ffb66768baa9b77b2a14
Christian Brodbeck <christianmbrodbeck@gmail.com>
2024-08-16 12:15:52 -0400

    WIP:  sklearn wrappers

---
+++ trftools/sklearn/__init__.py
@@ -0,0 +1,4 @@
+# Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
+"""Wrappers for using sklearn with NDVars"""
+from ._cluster import KMeans
+from ._decomposition import FastICA, PCA, SparsePCA
