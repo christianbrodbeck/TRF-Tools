5e439d6718db6c695e2bbc718c4b6d951e6e0bde
Christian Brodbeck <christianmbrodbeck@gmail.com>
2024-09-18 15:50:20 -0400

    RM gammatone_bank()
    numba dependency

--- trftools/__init__.py
+++ trftools/__init__.py
@@ -2,7 +2,6 @@
 from . import align, dictionaries, neural
 from ._ndvar import pad, shuffle
 from ._numpy_funcs import sin, cos, tan, arcsin, arccos, arctan, sinh, cosh, tanh, arcsinh, arccosh, arctanh
-from ._sound import gammatone_bank
 from .pipeline._eelfarm import start_dispatcher
 
 __version__ = '7'
